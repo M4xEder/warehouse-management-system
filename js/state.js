4187567e4c784cb34db78ac631ac2d98484cc917
Max_Elder <maxez1999@gmail.com>
2026-02-02 16:18:41 -0300

    Update state.js
     # Estado global + localStorage

--- js/state.js
+++ js/state.js
@@ -1,1 +1,20 @@
+// Estado global do sistema
+window.state = {
+  areas: [],
+  lotes: [],
+  historico: []
+};
 
+// Carregar do localStorage
+window.loadState = function () {
+  state.areas = JSON.parse(localStorage.getItem('areas')) || [];
+  state.lotes = JSON.parse(localStorage.getItem('lotes')) || [];
+  state.historico = JSON.parse(localStorage.getItem('historico')) || [];
+};
+
+// Salvar no localStorage
+window.saveState = function () {
+  localStorage.setItem('areas', JSON.stringify(state.areas));
+  localStorage.setItem('lotes', JSON.stringify(state.lotes));
+  localStorage.setItem('historico', JSON.stringify(state.historico));
+};
