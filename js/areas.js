054776c8c0f77816429ab3aa738a4d2b2490703e
Max_Elder <maxez1999@gmail.com>
2026-02-02 16:24:34 -0300

    Update areas.js
    Área

--- js/areas.js
+++ js/areas.js
@@ -1,1 +1,14 @@
+window.cadastrarArea = function () {
+  const nome = document.getElementById('areaNome').value.trim();
+  if (!nome) return alert('Informe o nome da área');
 
+  state.areas.push({
+    nome,
+    ruas: [
+      { posicoes: Array.from({ length: 10 }, () => ({ ocupada: false })) }
+    ]
+  });
+
+  saveState();
+  renderMapa();
+};
