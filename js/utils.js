c4a4ba338ca3873ed92ee07e757ea69d1057499d
Max_Elder <maxez1999@gmail.com>
2026-02-02 16:21:58 -0300

    Update utils.js
    Funções auxiliares 
    	cor
    	data
    	formatação
    	helpers

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,7 @@
+window.gerarCor = function () {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
 
+window.dataAtual = function () {
+  return new Date().toLocaleString('pt-BR');
+};
