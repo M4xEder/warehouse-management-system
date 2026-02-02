97abb8260a8f112bd5282b2b0283d6ab504a212b
Max_Elder <maxez1999@gmail.com>
2026-02-02 16:27:14 -0300

    Update busca.js
    Busca

--- js/busca.js
+++ js/busca.js
@@ -1,1 +1,13 @@
+window.buscar = function () {
+  const termo = document.getElementById('buscaInput').value.toLowerCase();
+  document.querySelectorAll('.posicao').forEach(p => {
+    p.classList.remove('highlight');
+    if (termo && p.dataset?.info?.includes(termo)) {
+      p.classList.add('highlight');
+    }
+  });
+};
 
+window.limparBusca = function () {
+  document.querySelectorAll('.posicao').forEach(p => p.classList.remove('highlight'));
+};
