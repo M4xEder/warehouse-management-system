11fd6b5af1e0f8287473d1e7efdfb78e5175e83d
Max_Elder <maxez1999@gmail.com>
2026-02-02 21:31:53 -0300

    Delete js/utils.js
    Delet

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-window.gerarCor = function () {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-};
-
-window.dataAtual = function () {
-  return new Date().toLocaleString('pt-BR');
-};
