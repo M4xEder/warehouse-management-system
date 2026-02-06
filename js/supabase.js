5bf79ba3923ad8afa21536d24d74b0e53ceb5f7b
Max_Elder <maxez1999@gmail.com>
2026-02-06 13:08:42 -0300

    Update supabase.js
    Integração

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
 
+// js/supabase.js
+const SUPABASE_URL = 'https://SEU-PROJETO.supabase.co';
+const SUPABASE_KEY = 'SUA-ANON-KEY';
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
