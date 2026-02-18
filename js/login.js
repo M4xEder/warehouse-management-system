d2c24e502ce88ded0ae4e382d57903bb134d8b87
Max_Elder <maxez1999@gmail.com>
2026-02-18 10:02:31 -0300

    Update login.js
    Chave key

--- js/login.js
+++ js/login.js
@@ -12,7 +12,7 @@ console.log('login.js carregado');
 
 const SUPABASE_URL = 'https://zegbjftbyckttcdrfwgf.supabase.co';
 
-const SUPABASE_KEY = 'SUA_PUBLIC_ANON_KEY_AQUI';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZ2JqZnRieWNrdHRjZHJmd2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNzAwNTAsImV4cCI6MjA4Njg0NjA1MH0.YdgrUkSnJ8ew2uMlU5Wdcd4k9iU4GSO0_vyNH1HT-lc';
 
 if (!window.supabase) {
   console.error('Supabase SDK não carregado.');
