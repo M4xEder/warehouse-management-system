9bc94d033d2fd28bd41431f97b527291530753ef
Max_Elder <maxez1999@gmail.com>
2026-02-19 19:27:33 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,3 @@
-const SUPABASE_URL = 'https://fctxvszjqhkfstzqgvat.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZ2JqZnRieWNrdHRjZHJmd2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNzAwNTAsImV4cCI6MjA4Njg0NjA1MH0.YdgrUkSnJ8ew2uMlU5Wdcd4k9iU4GSO0_vyNH1HT-lc';
 // =======================================
 // SUPABASE.JS — CLIENTE GLOBAL ÚNICO
 // =======================================
