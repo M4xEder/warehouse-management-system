79b866f7c6b98222cfcf7fed1a26c23fcd9d87c9
Max_Elder <maxez1999@gmail.com>
2026-02-19 03:38:22 -0300

    Update supabase.js
    Commite

--- js/supabase.js
+++ js/supabase.js
@@ -6,7 +6,7 @@
 const SUPABASE_URL = 'https://fctxvszjqhkfstzqgvat.supabase.co';
 
 // ⚠️ Use a chave "Publishable (anon public)" do painel
-const SUPABASE_ANON_KEY = 'SUA_CHAVE_ANON_AQUI';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZ2JqZnRieWNrdHRjZHJmd2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNzAwNTAsImV4cCI6MjA4Njg0NjA1MH0.YdgrUkSnJ8ew2uMlU5Wdcd4k9iU4GSO0_vyNH1HT-lc';
 
 // 🔥 CRIA CLIENTE GLOBAL
 window.supabaseClient = supabase.createClient(
