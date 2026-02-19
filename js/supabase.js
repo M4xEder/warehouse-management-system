5ffd195f004a0b8a1bb30167d88f918faca78dbb
Max_Elder <maxez1999@gmail.com>
2026-02-19 19:26:24 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,18 @@
 const SUPABASE_URL = 'https://fctxvszjqhkfstzqgvat.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZ2JqZnRieWNrdHRjZHJmd2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNzAwNTAsImV4cCI6MjA4Njg0NjA1MH0.YdgrUkSnJ8ew2uMlU5Wdcd4k9iU4GSO0_vyNH1HT-lc';
+// =======================================
+// SUPABASE.JS — CLIENTE GLOBAL ÚNICO
+// =======================================
 
-window.supabaseClient = createClient(
+console.log('supabase.js carregado');
+
+// ⚠️ COLOQUE AQUI O MESMO PROJETO QUE VOCÊ USA NO LOGIN
+const SUPABASE_URL = 'https://zegbjftbyckttcdrfwgf.supabase.co';
+
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZ2JqZnRieWNrdHRjZHJmd2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNzAwNTAsImV4cCI6MjA4Njg0NjA1MH0.YdgrUkSnJ8ew2uMlU5Wdcd4k9iU4GSO0_vyNH1HT-lc';
+
+// Cliente global
+window.supabase = supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
