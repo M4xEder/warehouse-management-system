52e80672ed3bb02c970de676598388a3e6698620
Max_Elder <maxez1999@gmail.com>
2026-02-21 08:49:03 -0300

    Update supabase.js
    Commit

--- js/supabase.js
+++ js/supabase.js
@@ -5,8 +5,8 @@
 console.log('supabase.js carregado');
 
 // ⚠️ COLOQUE SUAS CHAVES AQUI
-const SUPABASE_URL = 'SUA_URL_AQUI';
-const SUPABASE_ANON_KEY = 'SUA_CHAVE_AQUI';
+PROJET URL = 'https://zegbjftbyckttcdrfwgf.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZ2JqZnRieWNrdHRjZHJmd2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNzAwNTAsImV4cCI6MjA4Njg0NjA1MH0.YdgrUkSnJ8ew2uMlU5Wdcd4k9iU4GSO0_vyNH1HT-lc';
 
 // Criar client UMA ÚNICA VEZ
 window.supabaseClient = window.supabase.createClient(
