e8f4996eaf032e253fa448808ad29920262ec16d
Max_Elder <maxez1999@gmail.com>
2026-02-21 08:49:52 -0300

    Fix variable declaration for PROJET URL
    Const

--- js/supabase.js
+++ js/supabase.js
@@ -5,7 +5,7 @@
 console.log('supabase.js carregado');
 
 // ⚠️ COLOQUE SUAS CHAVES AQUI
-PROJET URL = 'https://zegbjftbyckttcdrfwgf.supabase.co';
+const PROJET URL = 'https://zegbjftbyckttcdrfwgf.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InplZ2JqZnRieWNrdHRjZHJmd2dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyNzAwNTAsImV4cCI6MjA4Njg0NjA1MH0.YdgrUkSnJ8ew2uMlU5Wdcd4k9iU4GSO0_vyNH1HT-lc';
 
 // Criar client UMA ÚNICA VEZ
